eaddd6f1e8b3ae524db8ec053a6aa1639fbe1bb1
drewgaren <drewg29@gmail.com>
2012-08-19 20:21:31 -0400

    DexterJB Flyer Kernel
    Dexters JellyBean Kernel For Flyer

---
+++ include.org/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 9 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#9 PREEMPT Sun Jul 22 14:59:42 BST 2012"
+#define LINUX_COMPILE_TIME "14:59:42"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "webserv"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 26 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#26 PREEMPT Sun Jul 29 12:16:38 BST 2012"
+#define LINUX_COMPILE_TIME "12:16:38"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "webserv"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
